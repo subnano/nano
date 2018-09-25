0eb5316a5b5ddacbe9abffd213218ea5a1085b8f
subnano <mark@knoxroad.net>
2018-09-25 10:19:13 +0100

    Bump version to 2018.09.4

--- script/release.sh
+++ script/release.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 # establish the current project version
+echo "Determining current version ..."
 CURRENT_VER=$(mvn help:evaluate -Dexpression=project.version | grep -v '\[')
-echo "Current version: $CURRENT_VER"
+
+# Make sure version is sane
+if [[ ${CURRENT_VER} =~ ^[0-9]{4}.[0-9]{2}.[0-9]+$ ]] ;then
+    echo "Current version: $CURRENT_VER"
+elif [[ ${CURRENT_VER} =~ ^[0-9]{4}.[0-9]{2}.[0-9]+-SNAPSHOT$ ]] ;then
+    echo "Current version: $CURRENT_VER"
+else
+    echo "Invalid version format: $CURRENT_VER"
+    exit 1
+fi
 
 # Determine the base version from current date
 BASE_VER=`date +%Y.%02m`
 
 # if base doesn't match then assume first release this month
-if [[ $CURRENT_VER != *$BASE_VER* ]] ;then
+if [[ ${CURRENT_VER} != *$BASE_VER* ]] ;then
     BUILD_NUM=1
 
 # if it ends with -SNAPSHOT then use this build number
-elif [[ $CURRENT_VER == *"-SNAPSHOT"* ]] ;then
+elif [[ ${CURRENT_VER} == *"-SNAPSHOT"* ]] ;then
     BUILD_NUM=${CURRENT_VER/-SNAPSHOT/}
     BUILD_NUM=${BUILD_NUM/$BASE_VER./}
 
@@ -30,7 +40,7 @@ echo "Next development version: $DEV_VER"
 echo
 
 # Bump to next release version
-echo "Bump version to release version $RELEASE_VER .."
+echo "Setting version to release version $RELEASE_VER .."
 mvn -q versions:set -DnewVersion=$RELEASE_VER -DgenerateBackupPoms=false
 git add -A
 git commit -m "Bump version to $RELEASE_VER"
@@ -44,7 +54,7 @@ git push origin $RELEASE_VER
 echo
 
 # Now bump to next development version
-echo "Bump version to next development version $DEV_VER .."
+echo "Setting version to next development version $DEV_VER .."
 mvn -q versions:set -DnewVersion=$DEV_VER -DgenerateBackupPoms=false
 git add -A
 git commit -m "Bump version to $DEV_VER"
