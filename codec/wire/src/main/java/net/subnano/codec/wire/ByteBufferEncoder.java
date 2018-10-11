3725390cb82ad88eb84e5598fe41a6b9bb377179
mark <mark@knoxroad.net>
2018-10-11 12:54:15 +0100

    Fix compile error

--- codec/wire/src/main/java/net/subnano/codec/wire/ByteBufferEncoder.java
+++ codec/wire/src/main/java/net/subnano/codec/wire/ByteBufferEncoder.java
@@ -4,7 +4,34 @@ import java.nio.ByteBuffer;
 
 public class ByteBufferEncoder<T> implements BufferEncoder {
 
-    public void encode(T source, ByteBuffer target) {
+    private final ByteBuffer buffer;
+
+    public ByteBufferEncoder(ByteBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void addByte(byte value) {
+
+    }
+
+    @Override
+    public void addShort(int value) {
+
+    }
+
+    @Override
+    public void addInt(int value) {
+
+    }
+
+    @Override
+    public void addLong(long value) {
+
+    }
+
+    @Override
+    public void addString(String value) {
 
     }
 }
